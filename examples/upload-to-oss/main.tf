2099c5ba42f4f1918f31cc5afdeddc8ad8d35ed8
lexwong <lexwong@lexwongdeMacBook-Air.local>
2019-09-24 15:58:50 +0800

    imporve(oss object): update the module to the format of the new version. 

--- examples/upload-to-oss/main.tf
+++ examples/upload-to-oss/main.tf
@@ -1,13 +1,14 @@
 module "oss_bucket" {
-  source = "terraform-alicloud-modules/oss-bucket/alicloud"
+  source      = "terraform-alicloud-modules/oss-bucket/alicloud"
   bucket_name = "bucket-terraform-20181111"
-  acl    = "private"
+  acl         = "private"
 }
 
 // Upload file without encryption
 module "oss-object" {
   source     = "../.."
-  bucketName = "${module.oss_bucket.this_oss_bucket_id}"
+  bucketName = module.oss_bucket.this_oss_bucket_id
   sourceFile = "test.txt"
   object-key = "test-1.txt"
 }
+
--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-resource "alicloud_oss_bucket_object" "this" {
-  bucket = "${var.bucketName}"
-  key    = "${var.object-key}"
-  source = "${var.sourceFile}"
+provider "alicloud" {
+  version              = ">=1.56.0"
+  region               = var.region != "" ? var.region : null
+  configuration_source = "terraform-alicloud-modules/oss-object"
 }
+
+resource "alicloud_oss_bucket_object" "this" {
+  bucket = var.bucketName
+  key    = var.object-key
+  source = var.sourceFile
+}
+
